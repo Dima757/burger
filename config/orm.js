42b46a44df9c5bd9236a84fcc29e4a8925f300b2
Dima <dima757@hotmail.com>
2018-12-01 13:34:21 -0600

    fourthcommit

--- config/orm.js
+++ config/orm.js
@@ -3,17 +3,17 @@ var connection = require('../config/connection.js')
 
 
 
-//Create the methods that will execute the necessary MySQL commands in the controllers. 
-//These are the methods you will need to use in order to retrieve and store data in your database.
+
+
 
 var orm = 
 {
 
-	//selectAll()
+	
 
 	selectAll: function(callback) 
 	{
-		//mySQL Query
+		
 		connection.query('SELECT * FROM burgers', function(err, result)
 		{
 			if (err) throw err;
@@ -21,7 +21,7 @@ var orm =
 		});
 	},
 
-	//insertOne()
+	
 	insertOne: function(burger_name, callback)
 	{
 		connection.query('INSERT INTO burgers SET ?', 
@@ -35,7 +35,7 @@ var orm =
 				
 	},
 
-	//updateOne()
+	
 	updateOne: function(burgerID, callback)
 	{
 		connection.query('UPDATE burgers SET ? WHERE ?', [{devoured: true}, {id: burgerID}],
@@ -48,5 +48,5 @@ var orm =
 };
 
 
-// Export the ORM object in module.exports.
+
 module.exports = orm;
